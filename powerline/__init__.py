943f4119af80595caaf81305f7779341cc1adb10
Zachary Bush <zach@zmbush.com>
2013-04-14 22:01:18 -0700

    powerline is go.

--- powerline/__init__.py
+++ powerline/__init__.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-from powerline.line import Line, Segment
+from powerline import line
 
 
 class Powerline(object):
     def __init__(self, mode, shell):
-        self.line = Line(mode, shell)
+        self.line = line.Line(mode, shell)
 
     def append(self, content, fg, bg, separator=None, separator_fg=None):
-        self.line.append(Segment(self.line, content, fg, bg, separator,
-                                 separator_fg))
+        self.line.append(line.Segment(self.line, content, fg, bg,
+                                      separator, separator_fg))
 
     def draw(self):
         self.line.draw()
